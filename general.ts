f88ffe5c1dc81d0198f752d5d85e21ab7586ef1f
Dany <zdanymc@gmail.com>
2024-11-29 20:22:10 +0100

    feat: generalize regions

--- general.ts
+++ general.ts
@@ -1,11 +1,11 @@
 import * as cheerio from 'cheerio'
 import { parse } from 'node:path'
 
-const homeUrl = 'https://www.serebii.net/pokemonhome'
-
-const regions = ['Kanto', 'Johto', 'Hoenn', 'Sinnoh', 'Unova', 'Kalos', 'Alola', 'Galar', 'Hisui', 'Paldea', 'Unknown']
-
-const formInName = ['Gigantamax ', 'Mega ', 'Primal ']
+interface Region {
+    index: number
+    href: string
+    name: string
+}
 
 interface Pokemon {
     index: number
@@ -18,7 +18,25 @@ interface Pokemon {
     types: string[]
 }
 
+// Base URL for the Pokémon Home section of Serebii
+const homeUrl = 'https://www.serebii.net/pokemonhome'
+// They have a trailing space to avoid matching with the pokemon name
+const formInName = ['Gigantamax ', 'Mega ', 'Primal ']
+
 const values: Pokemon[] = []
+
+async function fetchRegions(): Promise<Region[]> {
+    const regionsHtml = await fetch(`${homeUrl}/pokemon.shtml`).then(res => res.text())
+    const $ = cheerio.load(regionsHtml)
+    const regions = $('main>div').find('a')
+    return regions.map((i, el) => ({
+        index: i,
+        href: $(el).attr('href') ?? '',
+        name: $(el).text().trim()
+    })).toArray()
+}
+
+const regions = await fetchRegions()
 
 async function fetchGenderDiffs() {
     const html = await fetch('https://www.serebii.net/games/gender.shtml').then(res => res.text())
@@ -37,9 +55,8 @@ async function fetchGenderDiffs() {
 
 let genderDiffs = await fetchGenderDiffs()
 
-async function fetchPokemons(region: number, selector = 'table') {
-    const url = `${homeUrl}/${regions[region].toLowerCase()}pokemon.shtml`
-    const html = await fetch(url).then(res => res.text())
+async function fetchPokemons(region: Region, selector = 'table') {
+    const html = await fetch(`${homeUrl}/${region.href}`).then(res => res.text())
     const $ = cheerio.load(html)
     const table = $(selector)
     const rows = table.find('tr').slice(3).filter((_i, el) => $(el).find('td').length > 1)
@@ -59,7 +76,7 @@ async function fetchPokemons(region: number, selector = 'table') {
             || formInName.find(f => name.startsWith(f))?.trim() 
             || 'Default'
         if (form === 'Female') return
-        const item: Pokemon = { index, subIndex, region, depositable, ndex, name, form, types }
+        const item: Pokemon = { index, subIndex, region: region.index, depositable, ndex, name, form, types }
         values.push(item)
         const hasGenderDiff = genderDiffs.some(g => [ndex, img].includes(g))
         genderDiffs = genderDiffs.filter(g => ![ndex, img].includes(g))
@@ -68,14 +85,13 @@ async function fetchPokemons(region: number, selector = 'table') {
 }
 
 for (const region of regions) {
-    const index = regions.indexOf(region)
-    await fetchPokemons(index)
-    console.log(`${region} :`, values.filter(p => p.region === index).length)
+    await fetchPokemons(region)
+    console.log(`${region.name} :`, values.filter(p => p.region === region.index).length)
 }
 
 // Sort by index and then by subIndex
 values.sort((a, b) => a.index === b.index ? a.subIndex - b.subIndex : a.index - b.index)
 
-console.log('Total Pokémons :', values.length)
+console.log('\nTotal Pokémons :', values.length)
 
 Bun.write('./pokedex.json', JSON.stringify(values, null, 2))
